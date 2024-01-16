22604b069edbdf0db12e78af3eb265e15f68f037
axymthr <mathur.aksh@gmail.com>
2024-01-16 23:05:22 +0530

    Project scaffolding and utils

--- README.md
+++ README.md
@@ -3,6 +3,82 @@
 Welcome to the Advent of Code[^aoc] Kotlin project created by [axymthr][github] using the [Advent of Code Kotlin Template][template] delivered by JetBrains.
 
 In this repository, axymthr is about to provide solutions for the puzzles using [Kotlin][kotlin] language.
+```
+.
+├── README.md               README file
+├── build.gradle.kts        Gradle configuration created with Kotlin DSL
+├── settings.gradle.kts     Gradle project settings
+├── gradle*                 Gradle wrapper files
+└── src
+├── Day01.kt            An empty implementation for the first AoC day
+├── Utils.kt            A set of utility methods shared across your days
+│                       
+│                       (create those files manually)
+├── Day01.txt           An empty file for the Day 01 input data
+└── Day01_test.txt      An optional Day 01 test input data used for checks
+```
+> [!NOTE]
+>
+> All task input files (`src/*.txt`) are excluded from the repository with `.gitignore` – we should not post them publicly, as [Eric Wastl requested for](https://twitter.com/ericwastl/status/1465805354214830081).
+
+When the first puzzle appears, go to the `Day01.kt`, and for each `part1` and `part2` function, provide an algorithm implementation using the `input` data loaded from the `src/Day01.txt` file.
+This input data is common for both parts, and you can find it at the bottom of each day on the [Advent of Code][aoc] page.
+
+To read the input data, you can go with the `readInput(name: String)` utility method provided in the [`Utils.kt`][file:utils] file, like:
+
+```kotlin
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput("Day01")
+    println(part1(input))
+}
+```
+
+## Running
+
+> [!IMPORTANT]
+>
+> Create relevant files Before running tasks or tests, like: `src/Day01.txt` or `src/Day01_test.txt`.
+
+The [`Utils.kt`][file:utils] file also contains the `String.md5()` method for generating MD5 hash out of the given string and expects more helper functions for the sake of the [KISS principle][kiss].
+
+Each puzzle describes some test conditions, a small portion of the information that helps check if the produced value for the given test input is valid.
+To handle that case, you can put such an input into a separate file and perform a check against the output, like:
+
+```kotlin
+fun main() {
+    // ...
+    
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 13)
+}
+```
+
+The current approach of providing both `part1` and `part2` solutions within the single `Day##.kt` file may sometimes bring a disadvantage due to the first solution calculation when we expect to work on the second part only.
+With simple cases that don't consume too much of your time and resources that can be almost unnoticeable, but when the solution takes seconds, it is worth considering breaking the daily solution into two separated pieces, like `Day07_part1.kt` and `Day07_part2.kt`.
+
+The final result of your algorithm will be printed on the screen so that you can pass it to the Advent of Code website.
+
+To go with the next day, place the `Day02.txt` file into the `src` with relevant input data and create a `Day02.kt` file with a similar code scaffold:
+
+```kotlin
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+```
 
 If you're stuck with Kotlin-specific questions or anything related to this template, check out the following resources:
 
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,132 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * https://blog.jetbrains.com/kotlin/2021/12/tips-and-tricks-for-solving-advent-of-code/
+ *
+ * Parses input lines.
+ * Use the destructured function to assign the output to different variables right away
+ * Library functions like substringBefore() and substringAfter() cover lots of cases and are often enough.
+ * Use String.toInt() and Char.digitToInt() to convert the string content or characters into integers.
+ */
+fun parseInput(s: String) {
+    val inputLineRegex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
+    val (startX, startY, endX, endY) = inputLineRegex
+        .matchEntire(s)
+        ?.destructured
+        ?: throw IllegalArgumentException("Incorrect input line $s")
+}
+
+/**
+ * It’s often helpful to introduce domain types, even for smaller tasks like these puzzles.
+ * data class Step(val direction: Direction, val units: Int)
+ * fun String.toStep(): Step
+ * fun String.toTicket(): Ticket
+ * fun String.toSevenSegmentDigitList(): List<SevenSegmentDigit>
+ * val steps = readInput(fileName).map(String::toStep)
+ *
+ * Enumerations instead of strings
+ * enum class Direction { UP, DOWN, FORWARD }
+ * data class Step(val direction: Direction, val units: Int)
+ *
+ * // "forward 8"
+ * fun String.toStep() = Step(
+ *    Direction.valueOf(substringBefore(" ").uppercase()),
+ *    substringAfter(" ").toInt()
+ * )
+ *
+ * for ((direction, units) in steps) {
+ *    when (direction) {
+ *        UP -> depth -= units
+ *        DOWN -> depth += units
+ *        FORWARD -> horizontalPosition += units
+ *    }
+ * }
+ *
+ * typealias Segment = Char
+ * data class SevenSegmentDigit(val segments: Set<Segment>) {
+ *    constructor(s: String): this(s.toSet())
+ *    override fun toString() = segments.toList().sorted().joinToString("")
+ * }
+ *
+ * Use buildList and buildMap functions to build data structures imperatively:
+ * val monsters: List<Position> = buildList {
+ *    for (y in 0..tile.size - Monster.height) {
+ *        for (x in 0..tile.size - Monster.width) {
+ *            if (monsterAt(tileRotation, x, y)) {
+ *                add(Position(x, y))
+ *            }
+ *        }
+ *    }
+ * }
+ *
+ * val occurrences: Map<Int, List<Segment>> =
+ *    segments.groupBy { countSegmentInAllDigits(it) }
+ * // {8=[a, b], 9=[c], 7=[d, g], 4=[e], 6=[f]}
+ *
+ * listOf("abc", "c", "ad", "bc", "ab", "ca")
+ *    .groupingBy(String::first)
+ *    .eachCount() // {a=3, e=2, d=1}
+ *
+ * val rulesMap: Map<Int, Rule> = rules.associateBy { it.index }
+ *
+ * val initialState: Map<Int, Node> = numbers.associateWith { Node(it) }
+ *
+ * val bagRuleMap: Map<Bag, List<Content>> = bagRules.associate { it.bag to it.contentList }
+ *
+ * numbers.windowed(2).count { (first, second) -> first < second }
+ * "abcde".windowed(2) // [ab, bc, cd, de]
+ *
+ * sumOf maxOf minOf
+ *
+ * fun RoundConfiguration.calculateScore(): Long {
+ *    val winner = listOf(playerA, playerB).maxByOrNull { it.size }!!
+ *    return winner
+ *        .cards()
+ *        .foldIndexed(0L) { index, acc, element ->
+ *            acc + (winner.size - index) * element
+ *        }
+ * }
+ *
+ * private fun checkRow(row: List<Int>, visited: Set<Int>) =
+ *    row.all { elem -> elem in visited }
+ *        .also { result -> log("Checking row $row $visited: $result") }
+ *
+ * val differences = input
+ *    .windowed(2)
+ *    .also(::log)
+ *    .map { (first, second) -> second - first }
+ *    .also(::log)
+ *    .sorted()
+ * make a habit of using your own small log function instead. This way, you only need to change it in one place to stop
+ * printing all of the intermediate values for your solution.
+ *
+ * Need a queue or a stack to implement an algorithm when solving the puzzle? Use ArrayDeque, a double-ended queue that
+ * provides quick access to both ends.
+ * fun dfs(board: Board, initial: Cell): Set<Cell> {
+ *    val visited = mutableSetOf<Cell>()
+ *    val queue = ArrayDeque<Cell>()
+ *    queue += initial
+ *    while (queue.isNotEmpty()) {
+ *        val cell = queue.removeFirst()
+ *        visited += cell
+ *        queue += board.getUpperNeighbors(cell)
+ *    }
+ *    return visited
+ * }
+ *
+ * Consider overloading get and set operators to simplify the code for working with your class.
+ * dataclass Board(val content: List<List<Int>>) {
+ *    operator fun get(cell: Cell) =
+ *        content.getOrNull(cell.i)?.getOrNull(cell.j)
+ * }
+ * Instead of writing board.content[cell.i][cell.j] for all invocations, you write board[cell]
+ * data class Line(val start: Point, val end: Point) {
+ *    operator fun contains(point: Point): Boolean { … }
+ * }
+ * inputLines.count { line -> point in line }
+ *
+ *
+ *
+ */
